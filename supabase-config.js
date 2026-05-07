a42887711cc3d6dd704d92ee0403696ead2197a9
MYMDO <thejfex@proton.me>
2026-05-07 15:22:48 +0300

    Підключено реальну Supabase БД

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Supabase configuration
 // REPLACE these with your own values from https://supabase.com/dashboard
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-const SUPABASE_KEY = 'YOUR_ANON_KEY'; // public anon key
+const SUPABASE_URL =  'https://jxsvcodctysphexpawnu.supabase.co/rest/v1/'; //'https://YOUR_PROJECT.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_9exTLxabIh3PUjUMLdwDRg_x9EE5VrK'; //'YOUR_ANON_KEY'; // public anon key
 
 // Initialize Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
